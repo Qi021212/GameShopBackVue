a8be3f8314b7e2083c3c46d45987413aa8702d89
Qi021212 <3377803605@qq.com>
2025-03-09 23:05:14 +0800

    初始化提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  
+</script>
+
+<template>
+  <!-- 测试案例 -->
+  <div>
+    <h1>App</h1>
+    <router-link to="/home">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
